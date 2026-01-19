cfdeeec0165db5d694b1be5655708ca5c083a338
Monika-Khadka <monikakhadka367@gmail.com>
2026-01-19 08:42:09 +0545

    Week 2 Assignment

--- Week 2 script.js
+++ Week 2 script.js
@@ -1,32 +1,7 @@
 function checkResult() {
+    let a = Number(document.getElementById("s1").value);
+    let b = Number(document.getElementById("s2").value);
 
-    // Store marks in an array
-    let marks = [
-        Number(document.getElementById("s1").value),
-        Number(document.getElementById("s2").value),
-        Number(document.getElementById("s3").value),
-        Number(document.getElementById("s4").value)
-    ];
-
-    let total = 0;
-
-    // Loop to calculate total
-    for (let i = 0; i < marks.length; i++) {
-        total += marks[i];
-    }
-
-    document.getElementById("sum").innerHTML = "Total Marks: " + total + " / 400";
-
-    // Decision making
-    let result;
-
-    if (total >= 300) {
-        result = "PASS – Excellent";
-    } else if (total >= 200) {
-        result = "PASS";
-    } else {
-        result = "FAIL";
-    }
-
-    document.getElementById("status").innerHTML = result;
+    document.getElementById("result").innerHTML = a + b;
 }
+
