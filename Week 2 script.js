b37b59e7c33055888b9c5f0654abe54a670a9b33
Monika-Khadka <monikakhadka367@gmail.com>
2026-01-19 09:23:40 +0545

    Week 2 Assignment

--- Week 2 script.js
+++
@@ -1,7 +0,0 @@
-function checkResult() {
-    let a = Number(document.getElementById("s1").value);
-    let b = Number(document.getElementById("s2").value);
-
-    document.getElementById("result").innerHTML = a + b;
-}
-
